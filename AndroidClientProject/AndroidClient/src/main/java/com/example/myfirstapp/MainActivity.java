39f8d6689c1288fda4c6b9016ea65fee33fda889
Alex Saltiel <alex.saltiel@gmail.com>
2013-11-27 00:21:44 -0500

    Created receiveFile function

--- AndroidClientProject/AndroidClient/src/main/java/com/example/myfirstapp/MainActivity.java
+++ AndroidClientProject/AndroidClient/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -27,6 +27,8 @@ import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -42,6 +44,7 @@ import com.squareup.wire.Wire;
 public class MainActivity extends Activity {
     public static final String SERVERIP = "192.168.56.101"; //your computer IP address should be written here
     public static final int SERVERPORT = 2001;
+    public static final String path = Environment.getExternalStorageDirectory().toString() + "/Download/";
     private TextView textView;
     private boolean isConnected = false;
 
@@ -175,6 +178,36 @@ public class MainActivity extends Activity {
             }
         }
 
+        public void receiveFile(Song song){
+            int numBytesToRecv = song.lenofsong;
+            byte data[] = new byte[numBytesToRecv];
+
+            try {
+                input.read(data, 0, numBytesToRecv);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            File file = new File(path + song.title);
+            if(!file.exists()){
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            // just to be safe
+            file.setWritable(true);
+
+            try {
+                FileOutputStream stream = new FileOutputStream(path + song.title);
+                stream.write(data);
+                stream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         public void sendHeader(Header header) {
             Log.d("TCP", "Sending header...");
             byte[] data = header.toByteArray();
@@ -225,7 +258,6 @@ public class MainActivity extends Activity {
 
             Header response = receiveHeader();
 
-            String path = Environment.getExternalStorageDirectory().toString() + "/Download/";
             File dir = new File(path);
             String file[] = dir.list();
             String songs[] = new String[file.length];
